51e9f6fd9e9a8555a53eb5689a9a1dd9d064c3a3
bill <3207996+gestrich@users.noreply.github.com>
2024-12-01 12:10:54 -0500

    Updates

--- Sources/SwiftEverywhere.swift
+++ Sources/SwiftEverywhere.swift
@@ -10,28 +10,20 @@ import SwiftyGPIO
 @main
 struct SwiftEverywhere {
     static func main() async throws -> Void {
-        Self.readGPIO()
         try await Task.self .sleep(for: .seconds(1))
-        Self.setGPIO()
-        Self.readGPIO()
+        Self.setGPIO(on: true)
+        try await Task.self .sleep(for: .seconds(1))
+        Self.setGPIO(on: false)
     }
     
-    static func readGPIO() {
-        let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPi4)
-        guard let gp = gpios[.P21] else {
-            print("Could not read GPIO")
-            return
-        }
-        print(gp.value)
-    }
-    
-    static func setGPIO() {
+    static func setGPIO(on: Bool) {
         let gpios = SwiftyGPIO.GPIOs(for:.RaspberryPi4)
         guard let gp = gpios[.P21] else {
             print("Could not read GPIO")
             return
         }
 	gp.direction = .OUT
-        gp.value =  1
+        gp.value = on ? 1 : 0
+        print(gp.value)
     }
 }
