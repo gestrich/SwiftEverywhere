9431d7692e7efff3fcea7313273141958a9f851c
Bill Gestrich <bill@foreflight.com>
2024-12-07 10:40:43 -0500

    Combine packages

--- SwiftEverywherePi/Package.swift
+++ SwiftEverywherePi/Package.swift
@@ -8,12 +8,12 @@ let package = Package(
     platforms: [.macOS(.v15)],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
-        .package(url: "https://github.com/gestrich/SwiftyGPIO", branch: "bugfix/2024-12-pi-memory-address")
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/gestrich/SwiftyGPIO", branch: "bugfix/2024-12-pi-memory-address"),
         // .package(path: "../SwiftyGPIO")
+        .package(url: "https://github.com/vapor/vapor.git", from: "4.99.3")
     ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .executableTarget(
             name: "SwiftEverywherePi",
             dependencies: [
@@ -21,5 +21,27 @@ let package = Package(
                 .product(name: "SwiftyGPIO", package: "SwiftyGPIO")
             ]
         ),
+        .executableTarget(
+            name: "App",
+            dependencies: [
+                .product(name: "Vapor", package: "vapor"),
+                .product(name: "NIOCore", package: "swift-nio"),
+                .product(name: "NIOPosix", package: "swift-nio"),
+            ],
+            swiftSettings: swiftSettings
+        ),
+        .testTarget(
+            name: "AppTests",
+            dependencies: [
+                .target(name: "App"),
+                .product(name: "XCTVapor", package: "vapor"),
+            ],
+            swiftSettings: swiftSettings
+        )
     ]
 )
+
+var swiftSettings: [SwiftSetting] { [
+//    .enableUpcomingFeature("DisableOutwardActorInference"),
+//    .enableExperimentalFeature("StrictConcurrency"),
+] }
