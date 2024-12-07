c2b11f10964343bd3dcb421c05d8136a539e465d
Bill Gestrich <bill@foreflight.com>
2024-12-07 07:25:11 -0500

    Start of iOS App

---
+++ SwiftEverywhereApp/SwiftEverywhereApp/SwiftEverywhereAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftEverywhereAppApp.swift
+//  SwiftEverywhereApp
+//
+//  Created by Bill Gestrich on 12/7/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftEverywhereAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftEverywhereApp/SwiftEverywhereAppTests/SwiftEverywhereAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftEverywhereAppTests.swift
+//  SwiftEverywhereAppTests
+//
+//  Created by Bill Gestrich on 12/7/24.
+//
+
+import Testing
+@testable import SwiftEverywhereApp
+
+struct SwiftEverywhereAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
