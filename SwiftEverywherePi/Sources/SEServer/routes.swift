6aec47b31c5bcf1ccbb6317f6d1dbc1369d810fb
Bill Gestrich <bill@foreflight.com>
2024-12-07 13:32:49 -0500

    Method for activating LED

--- SwiftEverywherePi/Sources/SEServer/routes.swift
+++ SwiftEverywherePi/Sources/SEServer/routes.swift
@@ -16,4 +16,28 @@ func routes(_ app: Application) throws {
         ledGPIO.direction = .OUT
         return ledGPIO.value == 1 ? true : false
     }
+    
+    app.post("led") { request in
+        guard let data = request.body.data else {
+            throw RoutesError.unexpectedBody
+        }
+        let state = try JSONDecoder().decode(LEDState.self, from: data)
+        let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi4)
+        guard let ledGPIO = gpios[.P21] else {
+            print("Could not read GPIO")
+            throw RoutesError.gpioError
+        }
+        ledGPIO.direction = .OUT
+        ledGPIO.value = state.on ? 1 : 0
+        return "Done"
+    }
+}
+
+struct LEDState: Codable {
+    let on: Bool
+}
+
+enum RoutesError: LocalizedError {
+    case unexpectedBody
+    case gpioError
 }
