8b60ba3804d03d370b06843535df3bdf91f42828
Bill Gestrich <bill@foreflight.com>
2024-12-07 13:26:41 -0500

    Return GPIO state

--- SwiftEverywherePi/Sources/SEServer/routes.swift
+++ SwiftEverywherePi/Sources/SEServer/routes.swift
@@ -1,3 +1,5 @@
+import SEGPIO
+@preconcurrency import SwiftyGPIO
 import Vapor
 
 func routes(_ app: Application) throws {
@@ -5,7 +7,13 @@ func routes(_ app: Application) throws {
         "It works!"
     }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
+    app.get("led") { req async -> Bool in
+        let gpios = SwiftyGPIO.GPIOs(for: .RaspberryPi4)
+        guard let ledGPIO = gpios[.P21] else {
+            print("Could not read GPIO")
+            return false
+        }
+        ledGPIO.direction = .OUT
+        return ledGPIO.value == 1 ? true : false
     }
 }
