54d9ff5898e89b7f3102e997551fffc5afd19a64
Bill Gestrich <79173438+bill-foreflight@users.noreply.github.com>
2024-12-15 13:47:41 -0500

    Build warning

--- SwiftEverywhere/Sources/SECommon/LightSensorReading.swift
+++ SwiftEverywhere/Sources/SECommon/LightSensorReading.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public struct LightSensorReading: Codable, Equatable {
+public struct LightSensorReading: Codable, Equatable, Sendable {
     public let uploadDate: Date
     public let value: Double
     
