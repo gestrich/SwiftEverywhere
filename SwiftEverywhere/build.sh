6e224ee9568c0f97835295c79ee09545cd745d2e
Bill Gestrich <79173438+bill-foreflight@users.noreply.github.com>
2024-12-15 19:16:06 -0500

    Update build script

---
+++ SwiftEverywhere/build.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+set -eu
+set -o errexit
+set -o pipefail
+set -o nounset
+
+# Check parameters
+if [ "$#" -ne 3 ]; then
+    echo "Usage: <build target name> <platform name> <github token>"
+    exit 1
+fi
+
+PRODUCT="$1"
+PLATFORM_NAME="$2"
+GITHUB_TOKEN="$3"
+
+# Write Github Token to a Netrc file
+./appendNetrc.sh netrc "github.com" user "$GITHUB_TOKEN"
+./appendNetrc.sh netrc "api.github.com" user "$GITHUB_TOKEN"
+
+BUILD_DIR=$(pwd)/.aws-sam/build-$PRODUCT
+
+# Build docker image - fetches source dependent swift packages too.
+DOCKER_BUILDKIT=1 COMPOSE_DOCKER_CLI_BUILD=1 docker build --platform $PLATFORM_NAME --progress=plain --ulimit nofile=65536:65536 --secret id=netrc,src=netrc -f Dockerfile . -t builder
+
+# Clear the .build directory
+# rm -rf $BUILD_DIR
+
+# Copy from Docker to build directory
+# The || was added for this strange error: cp: cannot create directory '/build-target/checkouts/soto/models/apis/guardduty': File exists
+docker run --platform $PLATFORM_NAME --rm -v $BUILD_DIR:/build-target -w /build-src builder bash -c "chmod -R u+rw /stage/.build && cp -R  -n -p /stage/.build/* /build-target || echo 'error'"
+#docker run --platform $PLATFORM_NAME --rm -v $BUILD_DIR:/build-target -w /build-src --user root builder bash -c "chmod -R u+rw /stage/.build && cp -R  -n -p /stage/.build/* /build-target || echo 'error'"
+# Prep local directories
+mkdir -p $BUILD_DIR/lambda
+
+# Compile application
+docker run --platform $PLATFORM_NAME --rm -v $BUILD_DIR:/build-target -v $(pwd):/build-src -w /build-src builder bash -c "swift build --product $PRODUCT -c release --build-path /build-target --skip-update --disable-automatic-resolution"
+
+# Copy swift dependencies
+docker run --platform $PLATFORM_NAME --rm -v $BUILD_DIR:/build-target -v $(pwd):/build-src -w /build-src builder bash -c "ldd '/build-target/release/$PRODUCT' | grep swift | cut -d' ' -f3 | xargs cp -Lv -t /build-target/lambda"
+
+# Copy binary to stage
+cp $BUILD_DIR/release/$PRODUCT $BUILD_DIR/lambda/bootstrap
+
+echo "Packaging to zip"
+zip --symlinks -j lambda.zip $BUILD_DIR/lambda/*
+
+echo "Copy build directory to directory for Github action artifacts to upload"
+cp -r $BUILD_DIR/lambda lambda
+
+exit 0
