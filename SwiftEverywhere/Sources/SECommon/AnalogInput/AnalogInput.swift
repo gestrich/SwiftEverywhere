ab2b5401b3811906458b9702d91413c01c62249c
Bill Gestrich <79173438+bill-foreflight@users.noreply.github.com>
2024-12-21 09:57:16 -0500

    Updates

--- SwiftEverywhere/Sources/SECommon/AnalogInput/AnalogInput.swift
+++ SwiftEverywhere/Sources/SECommon/AnalogInput/AnalogInput.swift
@@ -20,7 +20,7 @@ public struct AnalogInput: Sendable {
         case .temperatureTMP36Fahrenheit:
             let voltageReference = 3.3
             // Step 4: Convert temperatures
-            let temperatureC = (voltage.rawValue - 0.5) * 100
+            let temperatureC = (reading - 0.5) * 100
             let temperatureF = (temperatureC * 9.0 / 5.0) + 32.0
             return temperatureF
         }
